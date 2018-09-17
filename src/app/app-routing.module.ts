b2086eaa4901ba91609bf58eae2aed98d16970cc
NickRegnard <nikiliev23@gmail.com>
2018-09-17 15:41:42 +0200

    wadasd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,18 +13,14 @@ import { TestComponent} from './test/test.component';
 import { UserpageComponent } from './userpage/userpage.component';
 import { MovieComponent } from './movie/movie.component';
 import { CcEditComponent } from './userpage/cc-edit/cc-edit.component';
-//import { PaymentEditComponent } from './userpage/payment-edit/payment-edit.component';
 import { BankEditComponent } from './userpage/bank-edit/bank-edit.component';
 import { PpEditComponent } from './userpage/pp-edit/pp-edit.component';
 import { MovieDetailComponent } from './movie/movie-detail/movie-detail.component';
 import { AdminComponent } from './admin/admin.component';
 import { MovieControlComponent } from './admin/movie-control/movie-control.component';
-<<<<<<< HEAD
 import { MovieAddComponent } from './admin/movie-add/movie-add.component';
-=======
 import { UserEditComponent } from './admin/user-edit/user-edit.component';
 import { UserHistoryAdminComponent } from './admin/user-history/user-history-admin.component';
->>>>>>> a473a1eb90dbf528740188a9a758bd2e71803f5c
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
@@ -40,12 +36,9 @@ const routes: Routes = [
     { path: 'movie/:id', component: MovieDetailComponent},
     { path: 'admin', component: AdminComponent, canActivate: [ElevatedGuard]},
     { path: 'editMovie/:id', component: MovieControlComponent, canActivate: [ElevatedGuard]},
-<<<<<<< HEAD
     { path: 'addMovie', component: MovieAddComponent, canActivate: [ElevatedGuard]},
-=======
     { path: 'history/:id', component: UserHistoryAdminComponent, canActivate:[ElevatedGuard]},
     { path: 'editUser/:id', component: UserEditComponent, canActivate:[ElevatedGuard]},
->>>>>>> a473a1eb90dbf528740188a9a758bd2e71803f5c
     { path: '**', redirectTo: '/home', pathMatch: 'full' },
 ]
 
